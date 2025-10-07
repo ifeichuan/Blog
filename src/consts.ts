34775c82595735fab7390fe0fcacd5ce1c9a807b
Feichuan <3010817478@qq.com>
2025-10-07 17:15:49 +0800

    添加新的UI组件（Nav、Intro、Prism）并更新主题样式

--- src/consts.ts
+++ src/consts.ts
@@ -1,12 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+import { act } from "react";
+
 export const SITE_TITLE = "扉川川的 Blog";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 
 export const NavList = [
-  { label: "Home", href: "/", icon: "home" },
-  { label: "About", href: "/about", icon: "info" },
-  { label: "Article", href: "/article", icon: "file-alt" },
-  { label: "Archive", href: "/archive", icon: "archive" },
+  { label: "Posts", href: "/posts", icon:"" },
 ];
