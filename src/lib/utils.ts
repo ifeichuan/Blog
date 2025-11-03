1afc47291027c0d4d776933b46c197381774a401
Feichuan <3010817478@qq.com>
2025-11-03 12:14:12 +0800

    添加 mitt 作为事件发射器，更新组件以使用事件系统

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-
+import mitt from 'mitt'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const emitter = mitt();
+
+
