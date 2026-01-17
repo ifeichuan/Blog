e935fe47c06ebce55d53ddbd46ea25ace89e8822
Feichuan <feichuan05@gmail.com>
2026-01-18 00:54:25 +0800

    feat: 添加移动端支持，创建 MobileLayout 组件并重定向移动设备用户；优化 TOC 组件样式

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,17 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import mitt from 'mitt'
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export const emitter = mitt();
+
+
+export const isMobileDevice = () => {
+  return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
 
 
